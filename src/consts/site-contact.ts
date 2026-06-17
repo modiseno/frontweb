592b290a2cb41cc0b3abf70986373df75abeaea4
MauricioOrtiz <modisenoyarq@gmail.com>
2026-06-17 19:35:19 +0200

    actualizacion y correcciones de contenido

--- src/consts/site-contact.ts
+++ src/consts/site-contact.ts
@@ -3,6 +3,6 @@ export const siteContact = {
     address: "Calle 2A # 20",
     city: "Puerto Colombia, Atlántico, Colombia",
     phone: "(+57) 300 791 8979",
-    email: "modisenoyarq@gmail.com",
+    email: "gerencia@modisenoyarquitectura.com",
     whatsapp: "573007918979"
 }
